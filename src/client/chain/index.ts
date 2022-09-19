bbbdef4d464fb2805fb82501c88753b43344b700
jacksonmori755 <jacksonmori755@gmail.com>
2022-09-19 13:09:02 +0400

    uncomment grpc client

--- src/client/chain/index.ts
+++ src/client/chain/index.ts
@@ -1,6 +1,6 @@
-// export * from './ChainGrpcClient'
-// export * from './ChainRestClient'
-export * from './grpc'
-export * from './rest'
-export * from './transformers'
-export * from './types'
+export * from './ChainGrpcClient';
+export * from './ChainRestClient';
+export * from './grpc';
+export * from './rest';
+export * from './transformers';
+export * from './types';
