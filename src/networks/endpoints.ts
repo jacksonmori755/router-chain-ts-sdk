8b7a7bfff6aba866ff2b6f42d1ac10c8023791b8
jacksonmori755 <jacksonmori755@gmail.com>
2022-09-26 14:45:41 +0400

    fix: endpoints

--- src/networks/endpoints.ts
+++ src/networks/endpoints.ts
@@ -13,13 +13,13 @@ export const urlEndpointsTestnet: NetworkEndpoints = {
        };
 
 export const urlEndpointsDevnet: NetworkEndpoints = {
-         explorerGql: 'http://127.0.0.1:3000/gql/query',
-         lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
-         grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
-       };
+  explorerGql: 'http://13.233.183.24:3000/gql/query',
+  lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
+  grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
+};
 
 export const urlEndpointsLocal: NetworkEndpoints = {
-         explorerGql: 'http://13.233.183.24:3000/gql/query',
-         lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
-         grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
-       };
+  explorerGql: 'http://127.0.0.1:3000/gql/query',
+  lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
+  grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
+};
