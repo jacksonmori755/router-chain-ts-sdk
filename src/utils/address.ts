8e4334cf2c0cd32d5c5e95a73a388c55565a47f5
jacksonmori755 <jacksonmori755@gmail.com>
2022-10-13 03:36:47 +0530

    validatorToDelegatorAddress function added

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,10 +1,14 @@
 import { bech32 } from 'bech32';
 import { Address } from 'ethereumjs-util';
 
+export const ROUTER_CHAIN_PREFIX = 'router';
+
+const CURRENT_CHAIN_PREFIX = ROUTER_CHAIN_PREFIX;
+
 export const getRouterAddress = (address: string): string => {
   const addressBuffer = Address.fromString(address.toString()).toBuffer();
 
-  return bech32.encode('router', bech32.toWords(addressBuffer));
+  return bech32.encode(CURRENT_CHAIN_PREFIX, bech32.toWords(addressBuffer));
 };
 
 export const getAddressFromRouterAddress = (address: string): string => {
@@ -24,4 +28,9 @@ export const isValidAddress = (address: string) => {
   } catch {
     return false;
   }
+};
+
+export const validatorToDelegatorAddress = (address: string) => {
+  const decode = bech32.decode(address).words;
+  return bech32.encode(CURRENT_CHAIN_PREFIX, decode);
 };
