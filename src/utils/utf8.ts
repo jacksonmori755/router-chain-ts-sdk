f9fe48849ab6e86079bcdf0feedbfb84e0de50d8
jacksonmori755 <jacksonmori755@gmail.com>
2022-10-31 19:39:09 +0530

    update: utf 8

--- src/utils/utf8.ts
+++ src/utils/utf8.ts
@@ -1,17 +1,17 @@
-import {TextEncoder, TextDecoder } from "util"
+import { TextEncoder, TextDecoder } from 'text-encoding';
 
 export function toUtf8(str: string): Uint8Array {
-  return new TextEncoder().encode(str)
+  return new TextEncoder().encode(str);
 }
 
 export function fromUtf8(data: Uint8Array): string {
-  return new TextDecoder('utf-8', { fatal: true }).decode(data)
+  return new TextDecoder('utf-8').decode(data);
 }
 
 export function toBase64(data: Record<string, any>): string {
-  return Buffer.from(JSON.stringify(data)).toString('base64')
+  return Buffer.from(JSON.stringify(data)).toString('base64');
 }
 
 export function fromBase64(payload: string): Record<string, any> {
-  return JSON.parse(Buffer.from(payload, 'base64').toString())
+  return JSON.parse(Buffer.from(payload, 'base64').toString());
 }
