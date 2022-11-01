b29145fe8e4e23958a8a510b0a0df3af11d9075f
jacksonmori755 <jacksonmori755@gmail.com>
2022-11-01 20:17:36 +0530

    update: change endpoints

--- src/networks/endpoints.ts
+++ src/networks/endpoints.ts
@@ -1,22 +1,22 @@
 import { NetworkEndpoints } from './types'
 
 export const urlEndpointsMainnet: NetworkEndpoints = {
-         explorerGql: 'http://127.0.0.1:3000/gql/query',
-         lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
-         grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
-       };
-
-export const urlEndpointsTestnet: NetworkEndpoints = {
-         explorerGql: 'http://127.0.0.1:3000/gql/query',
-         lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
-         grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
-       };
-
-export const urlEndpointsDevnet: NetworkEndpoints = {
-  explorerGql: 'http://13.233.183.24:3000/gql/query',
+  explorerGql: 'https://alpha-explorer-api.routerprotocol.com//gql/query',
   lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
   grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
 };
+
+export const urlEndpointsTestnet: NetworkEndpoints = {
+  explorerGql: 'https://alpha-explorer-api.routerprotocol.com//gql/query',
+  lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
+  grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
+};
+
+export const urlEndpointsDevnet: NetworkEndpoints = {
+         explorerGql: 'https://alpha-explorer-api.routerprotocol.com/gql/query',
+         lcdEndpoint: 'https://devnet-alpha.lcd.routerprotocol.com',
+         grpcEndpoint: 'https://devnet-alpha.grpcweb.routerprotocol.com',
+       };
 
 export const urlEndpointsLocal: NetworkEndpoints = {
   explorerGql: 'http://127.0.0.1:3000/gql/query',
