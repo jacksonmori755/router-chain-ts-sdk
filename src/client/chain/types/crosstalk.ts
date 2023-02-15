d0eaaf0caa7e725390b3b4524e7bca7bcc11748e
jacksonmori755 <jacksonmori755@gmail.com>
2023-02-15 10:54:21 +0400

    update: claim hash for crosstalk

--- src/client/chain/types/crosstalk.ts
+++ src/client/chain/types/crosstalk.ts
@@ -1,6 +1,8 @@
 import { sha256 } from "@cosmjs/crypto";
 import { CrossTalkRequest } from "@routerprotocol/chain-api/crosstalk/cross_talk_request_pb";
 
+import { CrossTalkRequestClaimHash } from "@routerprotocol/chain-api/crosstalk/cross_talk_request_pb";
+
 
 /**
  * Calculates claim hash by doing sha256 of CrossTalkRequest data members.
@@ -11,27 +13,36 @@ import { CrossTalkRequest } from "@routerprotocol/chain-api/crosstalk/cross_talk
  * @returns claim hash
  */
 export function getCrossTalkClaimHash(crossTalkRequest: CrossTalkRequest) : Uint8Array {
-    const path =
-        crossTalkRequest.getEventnonce() +
-        crossTalkRequest.getBlockheight() +
-        crossTalkRequest.getSourcechaintype() +
-        crossTalkRequest.getSourcechainid() +
-        crossTalkRequest.getSourcetxhash() +
-        crossTalkRequest.getDestinationchaintype() +
-        crossTalkRequest.getDestinationchainid() +
-        crossTalkRequest.getDestinationgaslimit() +
-        crossTalkRequest.getDestinationgasprice() +
-        crossTalkRequest.getRequestsender() +
-        crossTalkRequest.getRequestnonce() +
-        crossTalkRequest.getIsatomic() +
-        crossTalkRequest.getExpirytimestamp() +
-        crossTalkRequest.getDestcontractaddressesList_asB64().toString() +
-        crossTalkRequest.getDestcontractpayloadsList_asB64().toString() +
-        crossTalkRequest.getAcktype() +
-        crossTalkRequest.getAckgaslimit() +
-        crossTalkRequest.getAckgasprice();
+    
+    const claimHash = new CrossTalkRequestClaimHash();
 
-    const path_asU8 = path.split("").map(e => e.charCodeAt(0));
-    return sha256(new Uint8Array(path_asU8));
+    claimHash.setEventnonce(crossTalkRequest.getEventnonce()) 
+    claimHash.setBlockheight(crossTalkRequest.getBlockheight())  
+    claimHash.setSourcechaintype(crossTalkRequest.getSourcechaintype())
+    claimHash.setSourcechainid(crossTalkRequest.getSourcechainid() )
+    claimHash.setSourcetxhash(crossTalkRequest.getSourcetxhash())
+    claimHash.setDestinationchaintype(crossTalkRequest.getDestinationchaintype())
+    claimHash.setDestinationchainid(crossTalkRequest.getDestinationchainid())
+    claimHash.setDestinationgaslimit(crossTalkRequest.getDestinationgaslimit())  
+    claimHash.setDestinationgasprice(crossTalkRequest.getDestinationgasprice())  
+    claimHash.setRequestsender(crossTalkRequest.getRequestsender())
+    claimHash.setRequesttxorigin(crossTalkRequest.getRequesttxorigin())
+    claimHash.setIsreadcall(crossTalkRequest.getIsreadcall())
+    claimHash.setRequestnonce(crossTalkRequest.getRequestnonce())
+    claimHash.setIsatomic(crossTalkRequest.getIsatomic())
+    claimHash.setExpirytimestamp(crossTalkRequest.getExpirytimestamp())
+    claimHash.setDestcontractaddressesList(crossTalkRequest.getDestcontractaddressesList())  
+    claimHash.setDestcontractpayloadsList(crossTalkRequest.getDestcontractpayloadsList())
+    claimHash.setAcktype(crossTalkRequest.getAcktype()) 
+    claimHash.setAckgaslimit(crossTalkRequest.getAckgaslimit())
+    claimHash.setAckgasprice(crossTalkRequest.getAckgasprice())
+    claimHash.setFeepayer(crossTalkRequest.getFeepayer())
+
+    claimHash.toObject()
+
+    const marshalledClaimHash = JSON.stringify(claimHash)
+
+    const marshalledClaimHash_asU8 = marshalledClaimHash.split("").map(e => e.charCodeAt(0));
+    return sha256(new Uint8Array(marshalledClaimHash_asU8));
 }
 
