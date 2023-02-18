20f5962d47483636b16a5712279ef6fd6f574d84
jacksonmori755 <jacksonmori755@gmail.com>
2023-02-18 08:32:28 +0400

    update: renaming

--- src/client/chain/types/inbound.ts
+++ src/client/chain/types/inbound.ts
@@ -1,34 +1,44 @@
 import { InboundRequestClaimHash } from "@routerprotocol/chain-api/inbound/incoming_tx_pb";
 import { getClaimHash } from "./util";
 
-export function getMsgInboundRequestClaimHash(claimHashObject: InboundRequestClaimHash.AsObject) : Uint8Array {
+/**
+ * Get MsgInboundRequest ClaimHash
+ * @param msgInboundRequest 
+ * @returns 
+ */
+export function getMsgInboundRequestClaimHash(msgInboundRequest: InboundRequestClaimHash.AsObject) : Uint8Array {
     const claimHash = new InboundRequestClaimHash();
 
-    claimHash.setChaintype(claimHashObject.chaintype)
-    claimHash.setChainid(claimHashObject.chainid)
-    claimHash.setEventnonce(claimHashObject.eventnonce)
-    claimHash.setBlockheight(claimHashObject.blockheight)
-    claimHash.setSourcetxhash(claimHashObject.sourcetxhash)
-    claimHash.setSourcesender(claimHashObject.sourcesender)
-    claimHash.setRouterbridgecontract(claimHashObject.routerbridgecontract)
-    claimHash.setPayload(claimHashObject.payload)
-    claimHash.setGaslimit(claimHashObject.gaslimit)
+    claimHash.setChaintype(msgInboundRequest.chaintype)
+    claimHash.setChainid(msgInboundRequest.chainid)
+    claimHash.setEventnonce(msgInboundRequest.eventnonce)
+    claimHash.setBlockheight(msgInboundRequest.blockheight)
+    claimHash.setSourcetxhash(msgInboundRequest.sourcetxhash)
+    claimHash.setSourcesender(msgInboundRequest.sourcesender)
+    claimHash.setRouterbridgecontract(msgInboundRequest.routerbridgecontract)
+    claimHash.setPayload(msgInboundRequest.payload)
+    claimHash.setGaslimit(msgInboundRequest.gaslimit)
 
     return getClaimHash(claimHash.serializeBinary())
 }
 
-export function getIncomingTxClaimHash(claimHashObject: InboundRequestClaimHash.AsObject) : Uint8Array {
+/**
+ * Get IncomingTx ClaimHash
+ * @param incomingTx 
+ * @returns 
+ */
+export function getIncomingTxClaimHash(incomingTx: InboundRequestClaimHash.AsObject) : Uint8Array {
     const claimHash = new InboundRequestClaimHash();
 
-    claimHash.setChaintype(claimHashObject.chaintype)
-    claimHash.setChainid(claimHashObject.chainid)
-    claimHash.setEventnonce(claimHashObject.eventnonce)
-    claimHash.setBlockheight(claimHashObject.blockheight)
-    claimHash.setSourcetxhash(claimHashObject.sourcetxhash)
-    claimHash.setSourcesender(claimHashObject.sourcesender)
-    claimHash.setRouterbridgecontract(claimHashObject.routerbridgecontract)
-    claimHash.setPayload(claimHashObject.payload)
-    claimHash.setGaslimit(claimHashObject.gaslimit)
+    claimHash.setChaintype(incomingTx.chaintype)
+    claimHash.setChainid(incomingTx.chainid)
+    claimHash.setEventnonce(incomingTx.eventnonce)
+    claimHash.setBlockheight(incomingTx.blockheight)
+    claimHash.setSourcetxhash(incomingTx.sourcetxhash)
+    claimHash.setSourcesender(incomingTx.sourcesender)
+    claimHash.setRouterbridgecontract(incomingTx.routerbridgecontract)
+    claimHash.setPayload(incomingTx.payload)
+    claimHash.setGaslimit(incomingTx.gaslimit)
 
     return getClaimHash(claimHash.serializeBinary())
 }
--- src/client/chain/types/outbound.ts
+++ src/client/chain/types/outbound.ts
@@ -1,22 +1,28 @@
 import { OutboundAckClaimHash } from "@routerprotocol/chain-api/outbound/outbound_ack_pb";
 import { getClaimHash } from "./util";
 
-export function getMsgOutboundAckClaimHash(claimHashObject: OutboundAckClaimHash.AsObject) : Uint8Array {
+/**
+ * Get MsgOutboundAck ClaimHash
+ * 
+ * @param msgOutboundAck 
+ * @returns 
+ */
+export function getMsgOutboundAckClaimHash(msgOutboundAck: OutboundAckClaimHash.AsObject) : Uint8Array {
     const claimHash = new OutboundAckClaimHash();
 
-    claimHash.setChaintype(claimHashObject.chaintype)
-    claimHash.setChainid(claimHashObject.chainid)
-    claimHash.setEventnonce(claimHashObject.eventnonce)
-    claimHash.setBlockheight(claimHashObject.blockheight)
-    claimHash.setOutboundtxnonce(claimHashObject.outboundtxnonce)
-    claimHash.setOutboundtxrequestedby(claimHashObject.outboundtxrequestedby)
-    claimHash.setRelayerrouteraddress(claimHashObject.relayerrouteraddress)
-    claimHash.setDestinationtxhash(claimHashObject.destinationtxhash)
-    claimHash.setContractackresponses(claimHashObject.contractackresponses)
-    claimHash.setExecode(claimHashObject.execode)
-    claimHash.setExecstatus(claimHashObject.execstatus)
-    claimHash.setExecflagsList(claimHashObject.execflagsList)
-    claimHash.setExecdataList(claimHashObject.execdataList)
+    claimHash.setChaintype(msgOutboundAck.chaintype)
+    claimHash.setChainid(msgOutboundAck.chainid)
+    claimHash.setEventnonce(msgOutboundAck.eventnonce)
+    claimHash.setBlockheight(msgOutboundAck.blockheight)
+    claimHash.setOutboundtxnonce(msgOutboundAck.outboundtxnonce)
+    claimHash.setOutboundtxrequestedby(msgOutboundAck.outboundtxrequestedby)
+    claimHash.setRelayerrouteraddress(msgOutboundAck.relayerrouteraddress)
+    claimHash.setDestinationtxhash(msgOutboundAck.destinationtxhash)
+    claimHash.setContractackresponses(msgOutboundAck.contractackresponses)
+    claimHash.setExecode(msgOutboundAck.execode)
+    claimHash.setExecstatus(msgOutboundAck.execstatus)
+    claimHash.setExecflagsList(msgOutboundAck.execflagsList)
+    claimHash.setExecdataList(msgOutboundAck.execdataList)
 
     return getClaimHash(claimHash.serializeBinary())
 }
