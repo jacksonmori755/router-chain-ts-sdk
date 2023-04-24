1eb1265ff7a818e69f9351a0db83396f30d7ed58
jacksonmori755 <jacksonmori755@gmail.com>
2023-04-24 12:25:32 +0400

    add: temp claim hash for cross chain

---
+++ src/client/chain/types/crosschain.ts
@@ -0,0 +1,49 @@
+// TODO: New claim hash object is required
+// import { CrossChainClaimHash } from "@routerprotocol/chain-api/routerchain/crosschain/claim";
+// import { CrosschainRequest } from "@routerprotocol/chain-api/routerchain/crosschain/crosschain_request_pb";
+// import { CrosschainAckRequest } from "@routerprotocol/chain-api/routerchain/crosschain/crosschain_ack_request_pb";
+// import { getClaimHash } from "./util";
+
+// crosschainRequest: CrosschainRequest.AsObject
+export function getCrosschainClaimHash() : Uint8Array {
+    //const claimHash = new CrosschainRequest()
+
+    // claimHash.setSrcChainId(crosschainRequest.srcChainId)
+    // claimHash.setRequestIdentifier(crosschainRequest.requestIdentifier)
+    // claimHash.blockHeight(crosschainRequest.blockHeight)
+    // claimHash.sourceTxHash(crosschainRequest.sourceTxHash)
+    // claimHash.srcTimestamp(crosschainRequest.srcTimestamp)
+    // claimHash.srcTxOrigin(crosschainRequest.srcTxOrigin)
+    // claimHash.routeAmount(crosschainRequest.routeAmount)
+    // claimHash.routeRecipient(crosschainRequest.routeRecipient)
+    // claimHash.destChainId(crosschainRequest.destChainId)
+    // claimHash.destChainId(crosschainRequest.destChainId)
+    // claimHash.requestSender(crosschainRequest.requestSender)
+    // claimHash.requestMetadata(crosschainRequest.requestMetadata)
+    // claimHash.requestPacket(crosschainRequest.requestPacket)
+    // claimHash.srcChainType(crosschainRequest.srcChainType)
+    // claimHash.destChainType(crosschainRequest.destChainType)
+
+    return new Uint8Array([10])
+}
+
+export function getCrosschainAckClaimHash() : Uint8Array {
+    // const claimHash = new CrosschainAckRequest()
+
+    // claimHash.setAckSrcChainId(crosschainRequest.ackSrcChainId)
+    // claimHash.setAckRequestIdentifier(crosschainRequest.ackRequestIdentifier)
+    // claimHash.setBlockheight(crosschainRequest.blockHeight)
+    // claimHash.setDesttxhash(crosschainRequest.desttxhash)
+    // claimHash.setRelayerrouteraddress(crosschainRequest.relayerrouteraddress)
+    // claimHash.setAckDestChainId(crosschainRequest.ackDestChainId)
+    // claimHash.setRequestSender(crosschainRequest.requestSender)
+    // claimHash.setRequestidentifier(crosschainRequest.requestidentifier)
+    // claimHash.setAckSrcChainType(crosschainRequest.ackSrcChainType)
+    // claimHash.setAckDestChainType(crosschainRequest.ackDestChainType)
+    // claimHash.setExecdata(crosschainRequest.execdata)
+    // claimHash.setExecstatus(crosschainRequest.execstatus)
+    // claimHash.setEthsigner(crosschainRequest.ethsigner)
+    // claimHash.setSignature(crosschainRequest.signature)
+
+    return new Uint8Array([10])
+}
