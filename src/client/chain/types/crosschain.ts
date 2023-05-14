f4cb51efb06503fd47084a7aa8d611ea0d8dc557
jacksonmori755 <jacksonmori755@gmail.com>
2023-05-14 18:36:39 +0400

    add: claim hash for crosschain

--- src/client/chain/types/crosschain.ts
+++ src/client/chain/types/crosschain.ts
@@ -1,49 +1,65 @@
-// TODO: New claim hash object is required
-// import { CrossChainClaimHash } from "@routerprotocol/chain-api/routerchain/crosschain/claim";
-import { CrosschainRequest } from "@routerprotocol/chain-api/routerchain/crosschain/crosschain_request_pb";
-import { CrosschainAckRequest } from "@routerprotocol/chain-api/routerchain/crosschain/crosschain_ack_request_pb";
-// import { getClaimHash } from "./util";
+import { CrosschainRequestClaimHash } from "@routerprotocol/chain-api/routerchain/crosschain/crosschain_request_pb";
+import { CrosschainAckRequestClaimHash } from "@routerprotocol/chain-api/routerchain/crosschain/crosschain_ack_request_pb";
+import { CrosschainAckReceiptClaimHash } from "@routerprotocol/chain-api/routerchain/crosschain/crosschain_ack_receipt_pb";
+import { getClaimHash } from "./util";
+
 
 // crosschainRequest: CrosschainRequest.AsObject
-export function getCrosschainClaimHash(crosschainRequest: CrosschainRequest.AsObject) : Uint8Array {
-    //const claimHash = new CrosschainRequest()
+export function getCrosschainClaimHash(crosschainRequest: CrosschainRequestClaimHash.AsObject) : Uint8Array {
+    const claimHash = new CrosschainRequestClaimHash()
 
-    // claimHash.setSrcChainId(crosschainRequest.srcChainId)
-    // claimHash.setRequestIdentifier(crosschainRequest.requestIdentifier)
-    // claimHash.blockHeight(crosschainRequest.blockHeight)
-    // claimHash.sourceTxHash(crosschainRequest.sourceTxHash)
-    // claimHash.srcTimestamp(crosschainRequest.srcTimestamp)
-    // claimHash.srcTxOrigin(crosschainRequest.srcTxOrigin)
-    // claimHash.routeAmount(crosschainRequest.routeAmount)
-    // claimHash.routeRecipient(crosschainRequest.routeRecipient)
-    // claimHash.destChainId(crosschainRequest.destChainId)
-    // claimHash.destChainId(crosschainRequest.destChainId)
-    // claimHash.requestSender(crosschainRequest.requestSender)
-    // claimHash.requestMetadata(crosschainRequest.requestMetadata)
-    // claimHash.requestPacket(crosschainRequest.requestPacket)
-    // claimHash.srcChainType(crosschainRequest.srcChainType)
-    // claimHash.destChainType(crosschainRequest.destChainType)
+    claimHash.setSrcChainId(crosschainRequest.srcChainId)
+    claimHash.setRequestIdentifier(crosschainRequest.requestIdentifier)
+    claimHash.setBlockHeight(crosschainRequest.blockHeight)
+    claimHash.setSourceTxHash(crosschainRequest.sourceTxHash)
+    claimHash.setSrcTimestamp(crosschainRequest.srcTimestamp)
+    claimHash.setSrcTxOrigin(crosschainRequest.srcTxOrigin)
+    claimHash.setRouteAmount(crosschainRequest.routeAmount)
+    claimHash.setRouteRecipient(crosschainRequest.routeRecipient)
+    claimHash.setDestChainId(crosschainRequest.destChainId)
+    claimHash.setRequestSender(crosschainRequest.requestSender)
+    claimHash.setRequestMetadata(crosschainRequest.requestMetadata)
+    claimHash.setRequestPacket(crosschainRequest.requestPacket)
+    claimHash.setSrcChainType(crosschainRequest.srcChainType)
+    claimHash.setDestChainType(crosschainRequest.destChainType)
 
-    return new Uint8Array([10])
+    return getClaimHash(claimHash.serializeBinary())
 }
 
-export function getCrosschainAckClaimHash(crosschainAckRequest: CrosschainAckRequest.AsObject) : Uint8Array {
-    // const claimHash = new CrosschainAckRequest()
+export function getCrosschainAckClaimHash(crosschainAckRequest: CrosschainAckRequestClaimHash.AsObject) : Uint8Array {
+    const claimHash = new CrosschainAckRequestClaimHash()
 
-    // claimHash.setAckSrcChainId(crosschainRequest.ackSrcChainId)
-    // claimHash.setAckRequestIdentifier(crosschainRequest.ackRequestIdentifier)
-    // claimHash.setBlockheight(crosschainRequest.blockHeight)
-    // claimHash.setDesttxhash(crosschainRequest.desttxhash)
-    // claimHash.setRelayerrouteraddress(crosschainRequest.relayerrouteraddress)
-    // claimHash.setAckDestChainId(crosschainRequest.ackDestChainId)
-    // claimHash.setRequestSender(crosschainRequest.requestSender)
-    // claimHash.setRequestidentifier(crosschainRequest.requestidentifier)
-    // claimHash.setAckSrcChainType(crosschainRequest.ackSrcChainType)
-    // claimHash.setAckDestChainType(crosschainRequest.ackDestChainType)
-    // claimHash.setExecdata(crosschainRequest.execdata)
-    // claimHash.setExecstatus(crosschainRequest.execstatus)
-    // claimHash.setEthsigner(crosschainRequest.ethsigner)
-    // claimHash.setSignature(crosschainRequest.signature)
+    claimHash.setAckSrcChainId(crosschainAckRequest.ackSrcChainId)
+    claimHash.setAckRequestIdentifier(crosschainAckRequest.ackRequestIdentifier)
+    claimHash.setBlockheight(crosschainAckRequest.blockheight)
+    claimHash.setDesttxhash(crosschainAckRequest.desttxhash)
+    claimHash.setRelayerrouteraddress(crosschainAckRequest.relayerrouteraddress)
+    claimHash.setAckDestChainId(crosschainAckRequest.ackDestChainId)
+    claimHash.setRequestSender(crosschainAckRequest.requestSender)
+    claimHash.setRequestidentifier(crosschainAckRequest.requestidentifier)
+    claimHash.setAckSrcChainType(crosschainAckRequest.ackSrcChainType)
+    claimHash.setAckDestChainType(crosschainAckRequest.ackDestChainType)
+    claimHash.setFeeconsumed(crosschainAckRequest.feeconsumed)
+    claimHash.setExecdata(crosschainAckRequest.execdata)
+    claimHash.setExecstatus(crosschainAckRequest.execstatus)
 
-    return new Uint8Array([10])
+    return getClaimHash(claimHash.serializeBinary())
+}
+
+export function getCrosschainAckReceiptClaimHash(crosschainAckReceipt: CrosschainAckReceiptClaimHash.AsObject) : Uint8Array {
+    const claimHash = new CrosschainAckReceiptClaimHash()
+
+    claimHash.setAckreceiptsrcchainid(crosschainAckReceipt.acksrcchainid)
+    claimHash.setAckReceiptIdentifier(crosschainAckReceipt.ackrequestidentifier)
+    claimHash.setAckReceiptBlockheight(crosschainAckReceipt.ackReceiptBlockheight)
+    claimHash.setAckReceiptTxhash(crosschainAckReceipt.ackReceiptTxhash)
+    claimHash.setRelayerrouteraddress(crosschainAckReceipt.relayerrouteraddress)
+    claimHash.setRequestidentifier(crosschainAckReceipt.requestidentifier)
+    claimHash.setAcksrcchainid(crosschainAckReceipt.acksrcchainid)
+    claimHash.setRequestidentifier(crosschainAckReceipt.requestidentifier)
+    claimHash.setAcksrcchainid(crosschainAckReceipt.acksrcchainid)
+    claimHash.setAckrequestidentifier(crosschainAckReceipt.ackrequestidentifier)
+    claimHash.setFeeconsumed(crosschainAckReceipt.feeconsumed)
+
+    return getClaimHash(claimHash.serializeBinary())
 }
