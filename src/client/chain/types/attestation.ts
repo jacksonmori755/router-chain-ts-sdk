f24392e55e567ba5d618eabdb730e56fcae00848
jacksonmori755 <jacksonmori755@gmail.com>
2023-06-05 09:16:00 +0400

    fix: update chain-api

--- src/client/chain/types/attestation.ts
+++ src/client/chain/types/attestation.ts
@@ -1,29 +1,26 @@
-import { ValsetUpdatedClaimHash } from "@routerprotocol/chain-api/attestation/valset_updated_claim_pb";
-import { BridgeValidator } from "@routerprotocol/chain-api/attestation/bridge_validator_pb";
-import { getClaimHash } from "./util";
+import { ValsetUpdatedClaimHash } from '@routerprotocol/chain-api/attestation/valset_updated_claim_pb';
+import { BridgeValidator } from '@routerprotocol/chain-api/attestation/bridge_validator_pb';
+import { getClaimHash } from './util';
 
-/**
- * Get ValsetUpdated ClaimHash
- * @param valset 
- * @returns 
- */
-export function getValsetUpdatedClaimHash(valset: ValsetUpdatedClaimHash.AsObject) : Uint8Array {
-    const claimHash = new ValsetUpdatedClaimHash();
+export function getValsetUpdatedClaimHash(
+  claimHashObject: ValsetUpdatedClaimHash.AsObject
+): Uint8Array {
+  const claimHash = new ValsetUpdatedClaimHash();
 
-    let bridgeValidators: Array<BridgeValidator> = claimHashObject.membersList.map(
-      validatorObj => {
-        const bridgeValidator = new BridgeValidator();
-        bridgeValidator.setPower(validatorObj.power);
-        bridgeValidator.setEthereumaddress(validatorObj.ethereumaddress);
-        return bridgeValidator;
-      }
-    );
-    claimHash.setChainid(claimHashObject.chainid)
-    claimHash.setEventnonce(claimHashObject.eventnonce)
-    claimHash.setValsetnonce(claimHashObject.valsetnonce)
-    claimHash.setBlockheight(claimHashObject.blockheight)
-    claimHash.setMembersList(bridgeValidators)
-    claimHash.setSourcetxhash(valset.sourcetxhash)
+  let bridgeValidators: Array<BridgeValidator> = claimHashObject.membersList.map(
+    validatorObj => {
+      const bridgeValidator = new BridgeValidator();
+      bridgeValidator.setPower(validatorObj.power);
+      bridgeValidator.setEthereumaddress(validatorObj.ethereumaddress);
+      return bridgeValidator;
+    }
+  );
+  claimHash.setChainid(claimHashObject.chainid);
+  claimHash.setEventnonce(claimHashObject.eventnonce);
+  claimHash.setValsetnonce(claimHashObject.valsetnonce);
+  claimHash.setBlockheight(claimHashObject.blockheight);
+  claimHash.setMembersList(bridgeValidators);
+  claimHash.setSourcetxhash(claimHashObject.sourcetxhash);
 
-    return getClaimHash(claimHash.serializeBinary())
+  return getClaimHash(claimHash.serializeBinary());
 }
